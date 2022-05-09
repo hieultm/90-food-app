b96d56956f2192d39f37ac0ae828e7352fde627b
Dang Truong Giang <giangdt.kma@gmail.com>
2022-05-09 20:07:58 +0700

    set-up-build-tailwind

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
 import App from './App';
-import store from "./store/store"
-import { Provider } from "react-redux"
+import store from "./store/store";
+
+import './tailwind.css';
+import './tailwind.generated.css';
 
 
 ReactDOM.render(
